8cca4c32b4edc9cc5c4ad5191ecdc6ea05fb2a0b
PatrickDugan42 <patrickdugan42@gmail.com>
2025-03-05 13:50:32 -0500

    initial setup

---
+++ src/test/java/com/dugan/fetchreceiptprocessor/FetchReceiptProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dugan.fetchreceiptprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchReceiptProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
