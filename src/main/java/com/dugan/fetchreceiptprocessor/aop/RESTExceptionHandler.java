d9437c2cfa1904c95e47e649a0624ab51f0af63a
PatrickDugan42 <patrickdugan42@gmail.com>
2025-03-06 16:49:58 -0500

    Update RESTExceptionHandler.java
    Add invalid UUID exception handling

--- src/main/java/com/dugan/fetchreceiptprocessor/aop/RESTExceptionHandler.java
+++ src/main/java/com/dugan/fetchreceiptprocessor/aop/RESTExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.dugan.fetchreceiptprocessor.aop;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestControllerAdvice
 public class RESTExceptionHandler {
@@ -35,5 +38,33 @@ public class RESTExceptionHandler {
                         "status", 404,
                         "message", "No receipt found for that ID."
                 ));
+    }
+
+    //Handle status and message when the /receipts/{id}/points passed id is not a UUID
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidTypeForPathParameter(MethodArgumentTypeMismatchException exception,
+                                                                                 HttpServletRequest request) {
+        if(request.getRequestURI().matches("^/receipts/[^/]+/points$")){
+            String[] split = request.getRequestURI().split("/");
+            System.out.println(split.toString());
+            if(!isValidUUID(split[2])){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(Map.of(
+                                "status", 404,
+                                "message", "No receipt found for that ID."
+                        ));
+            }
+        }
+
+        throw exception;
+    }
+
+    private boolean isValidUUID(String value) {
+        try {
+            UUID.fromString(value); //try to cast to throw exception if not valid
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
