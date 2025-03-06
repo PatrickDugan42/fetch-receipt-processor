d42c69d7420b30e71232027344f2820e8f5b128e
PatrickDugan42 <patrickdugan42@gmail.com>
2025-03-06 14:20:21 -0500

    DTO update
    Added descriptions from the yml to the dto records

--- src/main/java/com/dugan/fetchreceiptprocessor/web/dto/ProcessRequest.java
+++ src/main/java/com/dugan/fetchreceiptprocessor/web/dto/ProcessRequest.java
@@ -4,6 +4,7 @@ package com.dugan.fetchreceiptprocessor.web.dto;
 
 
 import jakarta.validation.constraints.*;
+import org.springframework.context.annotation.Description;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -13,17 +14,22 @@ public record ProcessRequest(
         @Size(max = 255, message = "Retailer name must be under 256 characters.")
         @NotNull
         @NotEmpty
+        @Description("The name of the retailer or store the receipt is from.")
         String retailer,
         @NotNull
         @NotEmpty
+        @Description("The date of the purchase printed on the receipt.")
         String purchaseDate,
         @NotNull
         @NotEmpty
+        @Description("The time of the purchase printed on the receipt. 24-hour time expected.")
         String purchaseTime,
         @Size(min = 1)
+        @Description("The purchased items.")
         List<ProcessItem> items,
         @Pattern(regexp = "^\\d+\\.\\d{2}$", message = "The retailer must match the following regexp, ^\\d+\\.\\d{2}$")
         @NotNull
         @NotEmpty
+        @Description("The total amount paid on the receipt.")
         String total) {
 }
